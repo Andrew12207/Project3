4c30cc91ddd9b64948bbbe6eb43bc9cde1e009f3
Bntzer1 <bentz.ai@ufl.edu>
2025-04-21 02:43:20 -0400

    combine mergesort into one header file

--- src/MergeSort.h
+++ src/MergeSort.h
@@ -1,27 +1,65 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <chrono>
 
-struct Node {
-    int id;
-    std::string name;
-    std::string store;
-    float price;
-    Node* next;
-    
-    // Constructor
-    Node(int _id, std::string _name, std::string _store, float _price) : 
-        id(_id), name(_name), store(_store), price(_price), next(nullptr) {}
-};
+struct Node;
 
 class MergeSort {
 public:
-
-    static Node* mergeSort(Node* head);
+    static Node* mergeSort(Node* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        
+        Node* front = nullptr;
+        Node* back = nullptr;
+        
+        splitList(head, &front, &back);
+        
+        front = mergeSort(front);
+        back = mergeSort(back);
+        
+        return mergeLists(front, back);
+    }
     
 private:
-
-    static void splitList(Node* head, Node** frontRef, Node** backRef);
-    static Node* mergeLists(Node* a, Node* b);
+    static void splitList(Node* head, Node** frontRef, Node** backRef) {
+        if (head == nullptr || head->next == nullptr) {
+            *frontRef = head;
+            *backRef = nullptr;
+            return;
+        }
+        
+        Node* slow = head;
+        Node* fast = head->next;
+        
+        while (fast != nullptr) {
+            fast = fast->next;
+            if (fast != nullptr) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+        
+        *frontRef = head;
+        *backRef = slow->next;
+        slow->next = nullptr;
+    }
+    
+    static Node* mergeLists(Node* a, Node* b) {
+        if (a == nullptr) return b;
+        if (b == nullptr) return a;
+        
+        Node* result = nullptr;
+        
+        if (a->id <= b->id) {
+            result = a;
+            result->next = mergeLists(a->next, b);
+        } else {
+            result = b;
+            result->next = mergeLists(a, b->next);
+        }
+        
+        return result;
+    }
 };
